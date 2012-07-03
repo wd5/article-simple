eb764ee709dd4d5878e5b327a7f3e9d4045bb386
wd5 <developer@wd5.ru>
2012-07-03 17:24:55 +0400

    добавлены таблицы для статей и новостей

--- article/models.py
+++ article/models.py
@@ -1,7 +1,6 @@
 # -*- coding: UTF-8 -*-
 from django.db import models
 
-# Create your models here.
 class SimpleNews(models.Model):
     title = models.CharField(max_length = 300, verbose_name= (u'Название для поисковика')) # заголовок для поисковика
     description = models.CharField(max_length = 255, blank=True, verbose_name= (u'Описание для поисковика')) # описание для поисковиков
@@ -25,6 +24,34 @@ class SimpleNewsCategory(models.Model):
     fullcontent = models.TextField(verbose_name= (u'подробное содержание')) #  подробное содержимое новости
     contentdata = models.DateField(verbose_name= (u'дата публикации')) #  дата публикации
     CategoryKey = models.ForeignKey('SimpleNewsCategory')
+
+    def __unicode__(self):
+        result = self.title + str(self.contentdata)
+        return result
+
+class SingleArticle(models.Model):
+    title = models.CharField(max_length = 300, verbose_name= (u'Название для поисковика')) # заголовок для поисковика
+    description = models.CharField(max_length = 255, blank=True, verbose_name= (u'Описание для поисковика')) # описание для поисковиков
+    header = models.TextField(verbose_name= (u'Название для людей')) # заголовок для новости
+    newsimages = models.ImageField(upload_to="news", verbose_name= (u'рисунок')) # рисунок для новости
+    briefcontent = models.TextField(verbose_name= (u'краткое содержание')) # краткое содержимое новости
+    fullcontent = models.TextField(verbose_name= (u'подробное содержание')) #  подробное содержимое новости
+    contentdata = models.DateField(verbose_name= (u'дата публикации')) #  дата публикации
+    CategoryKey = models.ForeignKey('SingleArticleCategory')
+
+    def __unicode__(self):
+        result = self.title + str(self.contentdata)
+        return result
+
+class SingleArticleCategory(models.Model):
+    title = models.CharField(max_length = 300, verbose_name= (u'Название для поисковика')) # заголовок для поисковика
+    description = models.CharField(max_length = 255, blank=True, verbose_name= (u'Описание для поисковика')) # описание для поисковиков
+    header = models.TextField(verbose_name= (u'Название для людей')) # заголовок для новости
+    newsimages = models.ImageField(upload_to="news", verbose_name= (u'рисунок')) # рисунок для новости
+    briefcontent = models.TextField(verbose_name= (u'краткое содержание')) # краткое содержимое новости
+    fullcontent = models.TextField(verbose_name= (u'подробное содержание')) #  подробное содержимое новости
+    contentdata = models.DateField(verbose_name= (u'дата публикации')) #  дата публикации
+    CategoryKey = models.ForeignKey('SingleArticleCategory')
 
     def __unicode__(self):
         result = self.title + str(self.contentdata)
