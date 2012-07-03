8fa720b213419f26c519b4350abb84c960971064
wd5 <developer@wd5.ru>
2012-07-03 17:19:38 +0400

    добавлена категория

--- article/models.py
+++ article/models.py
@@ -15,3 +15,17 @@ class SimpleNews(models.Model):
     def __unicode__(self):
         result = self.title + str(self.contentdata)
         return result
+
+class SimpleNewsCategory(models.Model):
+    title = models.CharField(max_length = 300, verbose_name= (u'Название для поисковика')) # заголовок для поисковика
+    description = models.CharField(max_length = 255, blank=True, verbose_name= (u'Описание для поисковика')) # описание для поисковиков
+    header = models.TextField(verbose_name= (u'Название для людей')) # заголовок для новости
+    newsimages = models.ImageField(upload_to="news", verbose_name= (u'рисунок')) # рисунок для новости
+    briefcontent = models.TextField(verbose_name= (u'краткое содержание')) # краткое содержимое новости
+    fullcontent = models.TextField(verbose_name= (u'подробное содержание')) #  подробное содержимое новости
+    contentdata = models.DateField(verbose_name= (u'дата публикации')) #  дата публикации
+    CategoryKey = models.ForeignKey('SimpleNewsCategory')
+
+    def __unicode__(self):
+        result = self.title + str(self.contentdata)
+        return result
